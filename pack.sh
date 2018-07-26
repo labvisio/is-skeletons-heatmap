b7120f3f97a51492bc6338cf37d6bd1030573330
Felippe <mendonca.felippe@gmail.com>
2018-07-26 10:20:38 +0100

    + bash script to build and push docker image

---
+++ pack.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+docker_user="viros"
+image_tag="${docker_user}/is-skeletons-heatmap:1"
+docker build . -f Dockerfile -t ${image_tag} --network=host --no-cache
+read -r -p "Do you want to push image ${image_tag}? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
+    echo "Log-in as '${docker_user}' at Docker registry:"
+    docker login -u ${docker_user}
+    docker push ${image_tag}
+fi
